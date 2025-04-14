637e86cba7b4ea41520e1e92b44e0ac730dc964b
JamesWithGenie <jihoon.james.kim@gmail.com>
2025-04-14 11:53:22 +0900

    Delete components directory

--- components/ui/card.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export function Card({ children, className }: any) {
-  return <div className={`border ${className}`}>{children}</div>;
-}
-
-export function CardContent({ children }: any) {
-  return <div className="p-4">{children}</div>;
-}
