0e1808308e57dcef200bdd4658d071ca196483b2
JamesWithGenie <jihoon.james.kim@gmail.com>
2025-04-14 11:17:51 +0900

    Delete components directory

--- components/ui/button.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export function Button({ children, className }: any) {
-  return <button className={`px-4 py-2 rounded-xl font-semibold ${className}`}>{children}</button>;
-}
--- components/ui/card.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export function Card({ children, className }: any) {
-  return <div className={`bg-white border rounded-xl ${className}`}>{children}</div>;
-}
-
-export function CardContent({ children }: any) {
-  return <div className="p-4">{children}</div>;
-}
